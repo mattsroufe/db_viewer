3ed1e878259d1f0f9669af0bf41ef6203982d5c5
Matt Sroufe <mattsroufe@gmail.com>
2018-10-31 08:52:13 +1300

    adds ability to save queries

--- app.rb
+++ app.rb
@@ -1,17 +1,18 @@
 require 'sinatra'
 require 'mysql2'
 
-client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'SchoolSchedulingExample')
+database = 'SchoolSchedulingExample'
+client = Mysql2::Client.new(host: 'localhost', username: 'root', database: database)
 tables = client.query('SHOW TABLES', :as => :array).to_a.flatten
 
 get '/' do
   sql = ''
   results = []
-  erb :index, locals: {tables: tables, sql: sql, results: results}
+  erb :index, locals: {tables: tables, sql: sql, results: results, database: database}
 end
 
 get '/search' do
   sql = params['q']
   results = client.query(sql)
-  erb :index, locals: {tables: tables, sql: sql, results: results} 
+  erb :index, locals: {tables: tables, sql: sql, results: results, database: database}
 end
--- public/app.js
+++ public/app.js
@@ -1,13 +1,34 @@
-var input = document.querySelector('textarea');
-var searchForm = document.querySelector('#search-form');
-input.focus();
-var tmpStr = input.value;
-input.value = '';
-input.value = tmpStr;
+var database = document.body.dataset.database
+var input = document.querySelector('textarea')
+var searchForm = document.querySelector('#search-form')
+var saveQueryBtn = document.querySelector('#save-query')
+var ul = document.querySelector('ul')
+var tableHeading = document.querySelector('#tables-heading').closest('li')
+input.focus()
+var tmpStr = input.value
+input.value = ''
+input.value = tmpStr
 
-input.addEventListener("keydown", function(event) {
+input.addEventListener('keydown', function(event) {
   if (event.keyCode === 13) {
-    event.preventDefault();
-    searchForm.submit();
+    event.preventDefault()
+    searchForm.submit()
   }
 });
+
+saveQueryBtn.addEventListener('click', function(event) {
+  var query = document.getElementsByName('q')[0].value
+  localStorage.setItem([database,query.replace(/\s/g,'')].join(':'), query)
+  location.reload()
+});
+
+for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+  var sql = localStorage.getItem(localStorage.key(i))
+  var newItem = document.createElement('li')
+  var a = document.createElement('a');
+  var linkText = document.createTextNode(sql);
+  a.appendChild(linkText);
+  a.href = `/search?q=${sql}`;
+  newItem.appendChild(a)
+  ul.insertBefore(newItem, tableHeading)
+}
